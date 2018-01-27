74151ba16296fd911b3f8fe84a33d282d884fc9e
pkarnati2004 <pkarnati@mit.edu>
2018-01-27 16:35:26 -0500

    Adding web pages

--- web/main.py
+++ web/main.py
@@ -1,26 +1,36 @@
 from flask import Flask, render_template, request
+import pandas as pd
+import matplotlib
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello world, welcome to she-hacks!'
+    return "hello world, welcome to SheHacks!!!!! Standalone page for index!!!!"
 
 @app.route('/form')
 def form():
-    return render_template('form.html')
+    return render_template('index.html')
+
+@app.route('/plotlib')
+def plot():
+    return "plotlib"
+    
 
 @app.route('/submitted', methods=['POST'])
 def submitted_form():
     name = request.form['name']
     email = request.form['email']
-    site = request.form['site_url']
-    comments = request.form['comments']
+    gender = request.form['gender']
+    race = request.form['race']
     return render_template(
         'submitted_form.html',
         name=name,
         email=email,
-        site=site,
-        comments=comments)
+        gender=gender,
+        race=race)
 
+
+if __name__ == "__main__":
+    app.run(debug=True)
